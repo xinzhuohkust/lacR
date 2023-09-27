87a47e06c6267a829419b2bfed9a3b82ec6f594b
Hsincho <99251348+xinzhuohkust@users.noreply.github.com>
2023-09-27 13:57:02 +0800

    Update setup_lac.R

--- R/setup_lac.R
+++ R/setup_lac.R
@@ -8,53 +8,108 @@ getGlobal <- function(name) {
   get(name, envir = LAC_env)
 }
 
-setup_lac <- function(custom = FALSE, location = NULL, python = NULL) {
+use_conda <- purrr::safely(reticulate::use_condaenv)
 
-  system("cmd.exe /c where python", intern = TRUE) |>
-    purrr::map_chr(~ paste("Found the following Python installations:", ., "\n")) |>
-    message()
+use_vir <- purrr::safely(reticulate::use_virtualenv)
 
-  if (is.null(python)) {
-    reticulate::conda_create("condaenv_for_python2r", python_version = "3.11")
+answer <- readline(prompt = "Is this your first time using this? If yes, a virtual environment will be created: (yes/no): ")
 
-    reticulate::conda_install("condaenv_for_python2r", packages = c("lac"), pip = TRUE)
+if (tolower(answer) == "yes") {
+  print("Welcome! Let's get started.")
 
-    reticulate::use_condaenv("condaenv_for_python2r", required = TRUE)
+  setup_lac <- function(custom = FALSE, location = NULL, conda = NULL) {
+    system("cmd.exe /c where python", intern = TRUE) |>
+      purrr::map_chr(~ paste("Found the following Python installations:", ., "\n")) |>
+      message()
 
-    LAC <- reticulate::import("LAC")
-  } else {
-    reticulate::virtualenv_create("virenv_for_python2r", python = python)
+    if (is.null(python)) {
+      reticulate::conda_create("condaenv_for_python2r", python_version = "3.11")
 
-    reticulate::virtualenv_install("virenv_for_python2r", packages = c("lac"))
+      reticulate::conda_install("condaenv_for_python2r", packages = c("lac"), pip = TRUE)
 
-    reticulate::use_virtualenv("virenv_for_python2r", required = TRUE)
+      reticulate::use_condaenv("condaenv_for_python2r", required = TRUE)
 
-    LAC <- reticulate::import(module = "LAC")
-  }
-
-  if (custom == FALSE & !is.null(location)) {
-    stop("location is ignored unless custom is TRUE")
-  } else {
-    if (custom == TRUE) {
-      lac_seg <- LAC$LAC(mode = "seg")
-
-      lac_seg$load_customization(location)
-
-      lac_analysis <- LAC$LAC(mode = "lac")
-
-      lac_analysis$load_customization(location)
-
-      setGlobal("lac_seg", lac_seg)
-
-      setGlobal("lac_analysis", lac_analysis)
+      LAC <- reticulate::import("LAC")
     } else {
-      lac_seg <- LAC$LAC(mode = "seg")
+      reticulate::virtualenv_create("virenv_for_python2r", python = python)
 
-      lac_analysis <- LAC$LAC(mode = "lac")
+      reticulate::virtualenv_install("virenv_for_python2r", packages = c("lac"))
 
-      setGlobal("lac_seg", lac_seg)
+      reticulate::use_virtualenv("virenv_for_python2r", required = TRUE)
 
-      setGlobal("lac_analysis", lac_analysis)
+      LAC <- reticulate::import(module = "LAC")
+    }
+
+    if (custom == FALSE & !is.null(location)) {
+      stop("location is ignored unless custom is TRUE")
+    } else {
+      if (custom == TRUE) {
+        lac_seg <- LAC$LAC(mode = "seg")
+
+        lac_seg$load_customization(location)
+
+        lac_analysis <- LAC$LAC(mode = "lac")
+
+        lac_analysis$load_customization(location)
+
+        setGlobal("lac_seg", lac_seg)
+
+        setGlobal("lac_analysis", lac_analysis)
+      } else {
+        lac_seg <- LAC$LAC(mode = "seg")
+
+        lac_analysis <- LAC$LAC(mode = "lac")
+
+        setGlobal("lac_seg", lac_seg)
+
+        setGlobal("lac_analysis", lac_analysis)
+      }
     }
   }
+} else if (tolower(answer) == "no") {
+  message("Great! Let's continue.")
+
+  setup_lac <- function(custom = FALSE, location = NULL, conda = NULL) {
+    if (conda == TRUE) {
+      if (is.null(use_conda("condaenv_for_python2r", required = TRUE)$error)) {
+        message("Using the conda virtual environment `condaenv_for_python2r`")
+        LAC <- reticulate::import(module = "LAC")
+      }
+    } else {
+      if (is.null(use_vir("virenv_for_python2r", required = TRUE)$error)) {
+        message("Using the conda virtual environment `condaenv_for_python2r`.")
+        LAC <- reticulate::import(module = "LAC")
+      }
+    }
+
+    if (custom == FALSE & !is.null(location)) {
+      stop("location is ignored unless custom is TRUE.")
+    } else {
+      if (custom == TRUE) {
+        lac_seg <- LAC$LAC(mode = "seg")
+
+        lac_seg$load_customization(location)
+
+        lac_analysis <- LAC$LAC(mode = "lac")
+
+        lac_analysis$load_customization(location)
+
+        setGlobal("lac_seg", lac_seg)
+
+        setGlobal("lac_analysis", lac_analysis)
+      } else {
+        lac_seg <- LAC$LAC(mode = "seg")
+
+        lac_analysis <- LAC$LAC(mode = "lac")
+
+        setGlobal("lac_seg", lac_seg)
+
+        setGlobal("lac_analysis", lac_analysis)
+      }
+    }
+  }
+} else {
+  print("Sorry, I did not understand your input. Please answer with 'yes' or 'no'.")
 }
+
+

7fdc755e33f858313a9d62c80c596fabe17f2b5c
Hsincho <99251348+xinzhuohkust@users.noreply.github.com>
2023-10-12 19:36:49 +0800

    Update setup_lac.R

--- R/setup_lac.R
+++ R/setup_lac.R
@@ -1,119 +1,147 @@
 LAC_env <- new.env()
 
 setGlobal <- function(name, value) {
-  assign(name, value, envir = LAC_env)
+    assign(name, value, envir = LAC_env)
 }
 
 getGlobal <- function(name) {
-  get(name, envir = LAC_env)
+    get(name, envir = LAC_env)
 }
 
 setup_lac <- function(custom = FALSE, location = NULL, conda = TRUE, verbose = TRUE, python = NULL) {
-  use_conda <- purrr::safely(reticulate::use_condaenv)
+    if (!is.null(python)) {
+        reticulate::use_python(python)
 
-  use_vir <- purrr::safely(reticulate::use_virtualenv)
+        LAC <- reticulate::import("LAC")
 
-  setup_first <- function(custom = FALSE, location = NULL, conda = NULL) {
-    if (is.null(python)) {
-      reticulate::conda_create("condaenv_for_python2r", python_version = "3.11")
+        if (custom == TRUE) {
+            lac_seg <- LAC$LAC(mode = "seg")
 
-      reticulate::conda_install("condaenv_for_python2r", packages = c("lac"), pip = TRUE)
+            lac_seg$load_customization(location)
 
-      reticulate::use_condaenv("condaenv_for_python2r", required = TRUE)
+            lac_analysis <- LAC$LAC(mode = "lac")
 
-      LAC <- reticulate::import("LAC")
+            lac_analysis$load_customization(location)
+
+            setGlobal("lac_seg", lac_seg)
+
+            setGlobal("lac_analysis", lac_analysis)
+        } else {
+            lac_seg <- LAC$LAC(mode = "seg")
+
+            lac_analysis <- LAC$LAC(mode = "lac")
+
+            setGlobal("lac_seg", lac_seg)
+
+            setGlobal("lac_analysis", lac_analysis)
+        }
     } else {
-      reticulate::virtualenv_create("virenv_for_python2r", python = python)
+        use_conda <- purrr::safely(reticulate::use_condaenv)
 
-      reticulate::virtualenv_install("virenv_for_python2r", packages = c("lac"))
+        use_vir <- purrr::safely(reticulate::use_virtualenv)
 
-      reticulate::use_virtualenv("virenv_for_python2r", required = TRUE)
+        setup_first <- function(custom = FALSE, location = NULL, conda = NULL) {
+            if (is.null(python)) {
+                reticulate::conda_create("condaenv_for_python2r", python_version = "3.11")
 
-      LAC <- reticulate::import(module = "LAC")
+                reticulate::conda_install("condaenv_for_python2r", packages = c("lac"), pip = TRUE)
+
+                reticulate::use_condaenv("condaenv_for_python2r", required = TRUE)
+
+                LAC <- reticulate::import("LAC")
+            } else {
+                reticulate::virtualenv_create("virenv_for_python2r", python = python)
+
+                reticulate::virtualenv_install("virenv_for_python2r", packages = c("lac"))
+
+                reticulate::use_virtualenv("virenv_for_python2r", required = TRUE)
+
+                LAC <- reticulate::import(module = "LAC")
+            }
+
+            if (custom == FALSE & !is.null(location)) {
+                stop("location is ignored unless custom is TRUE")
+            } else {
+                if (custom == TRUE) {
+                    lac_seg <- LAC$LAC(mode = "seg")
+
+                    lac_seg$load_customization(location)
+
+                    lac_analysis <- LAC$LAC(mode = "lac")
+
+                    lac_analysis$load_customization(location)
+
+                    setGlobal("lac_seg", lac_seg)
+
+                    setGlobal("lac_analysis", lac_analysis)
+                } else {
+                    lac_seg <- LAC$LAC(mode = "seg")
+
+                    lac_analysis <- LAC$LAC(mode = "lac")
+
+                    setGlobal("lac_seg", lac_seg)
+
+                    setGlobal("lac_analysis", lac_analysis)
+                }
+            }
+        }
+
+        setup_after_crated_vir <- function(custom = FALSE, location = NULL, conda = TRUE) {
+            if (conda == TRUE) {
+                if (is.null(use_conda("condaenv_for_python2r", required = TRUE)$error)) {
+                    message("Using the conda virtual environment `condaenv_for_python2r`")
+                    LAC <- reticulate::import(module = "LAC")
+                }
+            } else {
+                if (is.null(use_vir("virenv_for_python2r", required = TRUE)$error)) {
+                    message("Using the virtual environment `virenv_for_python2r`.")
+                    LAC <- reticulate::import(module = "LAC")
+                }
+            }
+
+            if (custom == FALSE & !is.null(location)) {
+                stop("location is ignored unless custom is TRUE.")
+            } else {
+                if (custom == TRUE) {
+                    lac_seg <- LAC$LAC(mode = "seg")
+
+                    lac_seg$load_customization(location)
+
+                    lac_analysis <- LAC$LAC(mode = "lac")
+
+                    lac_analysis$load_customization(location)
+
+                    setGlobal("lac_seg", lac_seg)
+
+                    setGlobal("lac_analysis", lac_analysis)
+                } else {
+                    lac_seg <- LAC$LAC(mode = "seg")
+
+                    lac_analysis <- LAC$LAC(mode = "lac")
+
+                    setGlobal("lac_seg", lac_seg)
+
+                    setGlobal("lac_analysis", lac_analysis)
+                }
+            }
+        }
+
+        if (verbose == TRUE) {
+            system("cmd.exe /c where python", intern = TRUE) |>
+                purrr::map_chr(~ paste("Found the following Python installations:", ., "\n")) |>
+                message()
+        }
+
+        answer <- readline(prompt = "Is this your first time using this? If yes, a virtual environment will be created: (yes/no): ")
+
+        if (tolower(answer) == "yes") {
+            message("Welcome! Let's get started.")
+            setup_first(custom, location, conda)
+        } else if (tolower(answer) == "no") {
+            message("Great! Let's continue.")
+            setup_after_crated_vir(custom, location, conda)
+        } else {
+            message("Sorry, I did not understand your input. Please answer with 'yes' or 'no'.")
+        }
     }
-
-    if (custom == FALSE & !is.null(location)) {
-      stop("location is ignored unless custom is TRUE")
-    } else {
-      if (custom == TRUE) {
-        lac_seg <- LAC$LAC(mode = "seg")
-
-        lac_seg$load_customization(location)
-
-        lac_analysis <- LAC$LAC(mode = "lac")
-
-        lac_analysis$load_customization(location)
-
-        setGlobal("lac_seg", lac_seg)
-
-        setGlobal("lac_analysis", lac_analysis)
-      } else {
-        lac_seg <- LAC$LAC(mode = "seg")
-
-        lac_analysis <- LAC$LAC(mode = "lac")
-
-        setGlobal("lac_seg", lac_seg)
-
-        setGlobal("lac_analysis", lac_analysis)
-      }
-    }
-  }
-
-  setup_after_crated_vir <- function(custom = FALSE, location = NULL, conda = TRUE) {
-    if (conda == TRUE) {
-      if (is.null(use_conda("condaenv_for_python2r", required = TRUE)$error)) {
-        message("Using the conda virtual environment `condaenv_for_python2r`")
-        LAC <- reticulate::import(module = "LAC")
-      }
-    } else {
-      if (is.null(use_vir("virenv_for_python2r", required = TRUE)$error)) {
-        message("Using the virtual environment `virenv_for_python2r`.")
-        LAC <- reticulate::import(module = "LAC")
-      }
-    }
-
-    if (custom == FALSE & !is.null(location)) {
-      stop("location is ignored unless custom is TRUE.")
-    } else {
-      if (custom == TRUE) {
-        lac_seg <- LAC$LAC(mode = "seg")
-
-        lac_seg$load_customization(location)
-
-        lac_analysis <- LAC$LAC(mode = "lac")
-
-        lac_analysis$load_customization(location)
-
-        setGlobal("lac_seg", lac_seg)
-
-        setGlobal("lac_analysis", lac_analysis)
-      } else {
-        lac_seg <- LAC$LAC(mode = "seg")
-
-        lac_analysis <- LAC$LAC(mode = "lac")
-
-        setGlobal("lac_seg", lac_seg)
-
-        setGlobal("lac_analysis", lac_analysis)
-      }
-    }
-  }
-
-  if (verbose == TRUE) {
-    system("cmd.exe /c where python", intern = TRUE) |>
-      purrr::map_chr(~ paste("Found the following Python installations:", ., "\n")) |>
-      message()
-  }
-
-  answer <- readline(prompt = "Is this your first time using this? If yes, a virtual environment will be created: (yes/no): ")
-
-  if (tolower(answer) == "yes") {
-    message("Welcome! Let's get started.")
-    setup_first(custom, location, conda)
-  } else if (tolower(answer) == "no") {
-    message("Great! Let's continue.")
-    setup_after_crated_vir(custom, location, conda)
-  } else {
-    message("Sorry, I did not understand your input. Please answer with 'yes' or 'no'.")
-  }
 }
